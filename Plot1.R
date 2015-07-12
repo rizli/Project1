c74258d47e5bdb12725dfc30b1db4a09b57ebce0
rizli <avriananda@yahoo.com>
2015-07-13 00:42:30 +0700

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -11,5 +11,5 @@ data$date_time <- as.POSIXct(date_time)
 hist(data$Global_active_power, main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
-dev.copy(png, file="plot1.png", height=520, width=520)
+dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
